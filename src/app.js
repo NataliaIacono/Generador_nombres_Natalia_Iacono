4771031e0dd57413d3bdba7520e57fa27f6b01d5
Natalia Iacono <natalia.iacono.pr@gmail.com>
2024-07-10 18:38:15 +0200

    comentarios

--- src/app.js
+++ src/app.js
@@ -11,10 +11,10 @@ const itemConcatenados = [];
 
 //Un for por cada array para que lo recorra por completo. Cada vuelta del for arrojara un item del array.
 
-for (let i = 0; i < pronoun.length; i++) {
-  for (let j = 0; j < adj.length; j++) {
-    for (let k = 0; k < noun.length; k++) {
-      for (let a = 0; a < dominio.length; a++) { //con .push lo que hago es formar la concatenacion
+for (let i = 0; i < pronoun.length; i++) { //the //our
+  for (let j = 0; j < adj.length; j++) { //great //big
+    for (let k = 0; k < noun.length; k++) { // jogger //raccon
+      for (let a = 0; a < dominio.length; a++) { //con .push lo que hago es formar la concatenacion // .com 
         itemConcatenados.push(pronoun[i] + adj[j] + noun[k] + dominio[a]);
       }
     }
